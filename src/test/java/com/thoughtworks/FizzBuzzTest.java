cc14ddade9793cbfb5c3b39ea1b6713f902c836f
Qiqi Zhao <qiqzhao@thoughtworks.com>
2020-01-10 18:13:29 +0800

    feat: should return Buzz when given 5

--- src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -15,4 +15,9 @@ public class FizzBuzzTest {
     public void should_return_Fizz_when_3() {
         assertEquals("Fizz", new FizzBuzz().calculate(3));
     }
+
+    @Test
+    public void should_return_Buzz_when_5() {
+        assertEquals("Buzz", new FizzBuzz().calculate(5));
+    }
 }
