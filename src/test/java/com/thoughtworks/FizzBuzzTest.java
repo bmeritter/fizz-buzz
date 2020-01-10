58de2360e959d9415e4b1032065d56098ed59778
Qiqi Zhao <qiqzhao@thoughtworks.com>
2020-01-10 18:09:04 +0800

    chore: remove unused file & create fizzBuzz

---
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+package com.thoughtworks;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void should_return_1_when_1() {
+        assertEquals(2, 1 + 1);
+    }
+}
